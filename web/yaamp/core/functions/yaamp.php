aba80c76f4414a55b017071548424d0f4d4e34e1
dongpu1813 <42663880+dongpu1813@users.noreply.github.com>
2020-06-02 08:30:09 +0800

    Update yaamp.php

--- web/yaamp/core/functions/yaamp.php
+++ web/yaamp/core/functions/yaamp.php
@@ -5,6 +5,7 @@ function yaamp_get_algos()
 	/* Toggle Site Algos Here */
 	return array(
 		'sha256',
+		'sha256d',
 		'sha256t',
 		'scrypt',
 		'scryptn',
@@ -124,6 +125,7 @@ function getAlgoColors($algo)
 {
 	$a = array(
 		'sha256'	=> '#d0d0a0',
+		'sha256d'	=> '#d0d0v0',
 		'sha256t'	=> '#d0d0f0',
 		'scrypt'	=> '#c0c0e0',
 		'neoscrypt'	=> '#a0d0f0',
@@ -190,6 +192,7 @@ function getAlgoPort($algo)
 {
 	$a = array(
 		'sha256'	=> 3333,
+		'sha256d'	=> 3336,
 		'sha256t'	=> 3339,
 		'lbry'		=> 3334,
 		'scrypt'	=> 3433,
